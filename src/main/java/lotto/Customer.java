b05b41640a98185f42a255e6dd8ca4aa0bc40909
mipangg <morganjeong119@gmail.com>
2025-08-11 17:43:18 +0900

    feat(Customer): 각자 구매한 로또 리스트를 저장할 수 있는 Customer 클래스 생성

--- src/main/java/lotto/Customer.java
+++ src/main/java/lotto/Customer.java
@@ -1,4 +1,12 @@
 package lotto;
 
+import java.util.List;
+
 public class Customer {
+    private List<Lotto> lottos;
+
+    Customer(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
 }
