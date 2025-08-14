bb9a125fbb543258ee441ef4e2f67f769a33a70d
mipangg <morganjeong119@gmail.com>
2025-08-14 17:37:10 +0900

    feat(Clerk): 입력 메서드 구현

--- src/main/java/lotto/Clerk.java
+++ src/main/java/lotto/Clerk.java
@@ -1,4 +1,32 @@
 package lotto;
 
+import java.util.Scanner;
+
 public class Clerk {
+    private Scanner sc = new Scanner(System.in);
+
+    public String inputLottoPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return sc.nextLine().trim();
+    }
+
+    public String inputWinningLotto() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return sc.nextLine().trim();
+    }
+
+    public String inputBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return sc.nextLine().trim();
+    }
+
+    public void outputChange(int change) {
+        System.out.println("거스름돈: " + change);
+    }
+
+    public void outputWinningResult(Customer customer) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+    }
+
 }
